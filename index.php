40be9ff2ae510f3d5fc3c5bcda4c73ada9f80523
Emanuel Leutgeb <emanuel@leutgeb.com>
2022-02-20 23:30:31 +0100

    Add index

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+ <head>
+  <title>PHP Master Course</title>
+ </head>
+ <body>
+ <a href="./Introduction%20to%20PHP/01variables.php">Variables</a>
+ <a href="./Introduction%20to%20PHP/02strings.php">Strings</a>
+ <a href="./Introduction%20to%20PHP/03numbers.php">Numbers</a>
+ <a href="./Introduction%20to%20PHP/">Booleans</a>
+ <a href="./Introduction%20to%20PHP/">Arrays</a>
+ <a href="./Introduction%20to%20PHP/">Arrays</a>
+ <a href="./Introduction%20to%20PHP/">NULL</a>
+ <a href="./Introduction%20to%20PHP/">Concatenation</a>
+ <a href="./Introduction%20to%20PHP/">if Statements</a>
+ <a href="./Introduction%20to%20PHP/">Logical Operators</a>
+ </body>
+</html>
