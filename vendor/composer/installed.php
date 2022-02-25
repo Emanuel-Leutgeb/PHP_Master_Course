e108589b9232c881b6037140bb18c673727a03ab
Emanuel Leutgeb <emanuel@leutgeb.com>
2022-02-25 10:09:34 +0100

    Add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c28b76bee24e37da2f9667713bf15fcf8eae3174',
+        'name' => 'vendor_name/php_master_course',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/php_master_course' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c28b76bee24e37da2f9667713bf15fcf8eae3174',
+            'dev_requirement' => false,
+        ),
+    ),
+);
